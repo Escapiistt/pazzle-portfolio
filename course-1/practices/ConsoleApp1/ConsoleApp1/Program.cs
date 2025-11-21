b20fc184026a17b6f70e855cc434b7fb595ec053
Escapiistt <tartem176@gmail.com>
2025-11-21 19:53:16 +0300

    Добавлено: ConsoleApp1 — проект на C#

---
+++ course-1/practices/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Мне выпала Чиса в Wuthering Waves");
+Console.WriteLine("Так же я выбил её сигнатурное оружие");
+Console.WriteLine("И апнул первого героя в доте 2!");
